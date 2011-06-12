652ce4c09eecf466cfb4595adeeb2ada4c2e3c23
mengyao <zhaomengyao@gmail.com>
2011-06-12 13:46:13 -0400

    Get reference sequence and alignments in a certain region.

--- hmm.c
+++ hmm.c
@@ -8,20 +8,49 @@
 #include <zlib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bam.h"
+#include "faidx.h"
 
 int main (int argc, char * const argv[]) {
-	bamFile fp = bam_open(argv[1], "r");
+	/*
+	  Declarations for reference file *.fa
+	 */
+	int32_t i;
+	faidx_t* fai = fai_load(argv[1]);
+	char* ref_seq;
+	int len = 0;
+
+	/*
+	  Declarations for bam file *.bam
+	 */	
+	bamFile fp = bam_open(argv[2], "r");
 	bam_header_t* header;
-	bam1_t* b;
+	bam1_t* b = bam_init1();
 	header = bam_header_read(fp);
-	b = bam_init1();
-	while (bam_read1(fp, b) >= 0) {
-		char *s = bam_format1(header, b);
-		puts(s);
-		free(s);
+	
+	/*
+	 Declarations for bam index *.bai
+	 */
+	bam_index_t* idx = bam_index_load(argv[2]);
+	bam_iter_t bam_iter;
+
+	fprintf (stdout, "Reference sequences:\n");
+	for (i = 0; i < header->n_targets; i ++) {
+		char* coordinate = ":0-999";
+		char* region = strcat(header->target_name[i], coordinate);
+		ref_seq = fai_fetch(fai, region, &len); /* len is a return value */
+		fprintf (stdout, "%s\n", ref_seq);
+		bam_iter = bam_iter_query(idx, i, 0, 999);
+ 		while (bam_iter_read (fp, bam_iter, b) >= 0) {
+			char* read_seq = (char*)bam1_seq(b);
+			char* read_name = bam1_qname(b);
+			fprintf (stdout, "read name: %s\n", read_name);
+		}
+		bam_iter_destroy(bam_iter);
 	}
 	bam_destroy1(b);
+	bam_index_destroy(idx);
 	bam_header_destroy(header);
 	bam_close(fp);
 	return 0;
